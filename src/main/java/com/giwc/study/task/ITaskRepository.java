60b756207a06ebd1decb1e560c05737c5939ad6c
Gabriel <wenck13@gmail.com>
2023-10-12 19:44:44 -0300

    Get tasks by user

--- src/main/java/com/giwc/study/task/ITaskRepository.java
+++ src/main/java/com/giwc/study/task/ITaskRepository.java
@@ -4,9 +4,12 @@ import com.giwc.study.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+    List<TaskModel> findByIdUser(UUID idUser);
 
 }
